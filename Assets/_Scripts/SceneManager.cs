dd295c1e867b3d0ca7a246084ad3730d7afc6b78
SPKoon <wdmab1204@naver.com>
2019-07-12 15:37:01 +0900

    3번째 패턴 추가

--- Assets/_Scripts/SceneManager.cs
+++ Assets/_Scripts/SceneManager.cs
@@ -22,7 +22,8 @@ public class SceneManager : MonoBehaviour
         Invoke("SummonMob_2",4f);
         Invoke("SummonMob_2",4.5f);
         Invoke("SummonMob_2",5f);
-        Invoke("SummonBoss",8.0f);
+        Invoke("SummonMob_3",7f);
+        Invoke("SummonBoss",15.0f);
     }
 
     public void addScore(int score)
@@ -43,6 +44,14 @@ public class SceneManager : MonoBehaviour
 
     void SummonMob_2(){ Instantiate(mob_2,mob_2.transform.position,Quaternion.identity); }
 
+    void SummonMob_3(){
+        Vector2[] pos = new Vector2[3]{new Vector2(-2,5.5f), new Vector2(0,5.5f), new Vector2(2,5.5f)};
+        foreach(Vector2 p in pos){
+            Instantiate(mob_1,p,Quaternion.identity);
+        }
+    }
+
     void SummonBoss(){ Instantiate(boss,boss.transform.position,Quaternion.identity); }
+
     //테스트 주석
 }
