844d3b60047a7b982efed9833bac818c400632ee
SPKoon <wdmab1204@naver.com>
2019-07-27 01:19:21 +0900

    죽었을 때 애니메이션 수정

--- Assets/_Scripts/State.cs
+++ Assets/_Scripts/State.cs
@@ -8,11 +8,15 @@ public class State : MonoBehaviour
     
     [HideInInspector]
     public int hp;
-    private Animator anim;
     public string EnemyBulletTag;
-    void Start(){
+    private Animator anim;
+    private SceneManager sceneManager;
+    void Awake(){
+        Screen.SetResolution(600, 800, false);
+
         hp = MaxHp;
         anim = GetComponent<Animator>();
+        sceneManager = FindObjectOfType<SceneManager>();
     }
     
     private void OnTriggerEnter2D(Collider2D other)
@@ -21,17 +25,20 @@ public class State : MonoBehaviour
             hp -= 1;
         }
         if(hp<=0){
-            FindObjectOfType<SceneManager>().addScore(100);
             if(gameObject.CompareTag("Player")){ //플레이어라면 애니메이션 재생
-                anim.SetTrigger("die");
-                Debug.Log("죽은사람 플레이어");
-                FindObjectOfType<SceneManager>().GameEnd();
-                Destroy(this.gameObject, 0.4f);
+                sceneManager.GameEnd();
             }
             else{
-                Destroy(this.gameObject);
+                sceneManager.addScore(100);
             }
+            anim.SetTrigger("die");
+            Destroy(this.gameObject, 0.4f);
         }
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
     }
 }
 // 테스트테스트 깃헙 테스트
