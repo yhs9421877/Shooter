29dbb060a1e7fdaa34a407e79983e336d484a462
SPKoon <wdmab1204@naver.com>
2019-08-04 16:33:12 +0900

    백그라운드 및 구름 추가

--- Assets/_Scripts/ScrollManager.cs
+++ Assets/_Scripts/ScrollManager.cs
@@ -6,22 +6,19 @@ public class ScrollManager : MonoBehaviour
 {
     public float speed;
     public int imageCount = 3;
-    public float imageHeight = 10f;
-    private Vector3 originalPosition;
-    void Awake()
-    {
-        originalPosition = transform.position;
-    }
+    public float imageHeight = 900f;
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y < -(imageCount - 1) * imageHeight)
+        RectTransform rt = GetComponent<RectTransform>();
+        Debug.Log(rt.anchoredPosition.y < -(imageCount - 1) * imageHeight);
+        if (rt.anchoredPosition.y < -(imageCount - 1) * imageHeight)
         {
             Debug.Log("오리지날포지션");
-            transform.position = originalPosition;
+            rt.anchoredPosition = new Vector2(0f, 0f);
         }
-           
-        transform.Translate(new Vector3(0, speed, 0));
+
+        rt.anchoredPosition += Vector2.down * speed;
     }
 }
