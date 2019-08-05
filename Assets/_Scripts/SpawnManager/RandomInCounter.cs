4f1ded60b43f11aed29cae68653352dd67325d73
SPKoon <wdmab1204@naver.com>
2019-08-05 20:14:39 +0900

    RandomInCounter 클래스 생성, SpawnManager클래스에 파생

---
+++ Assets/_Scripts/SpawnManager/RandomInCounter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomInCounter : MonoBehaviour
+{
+    public GameObject[] mobs;
+    public GameObject boss;
+    public float intervalTime = 3f;
+    private float remainTime;
+    [HideInInspector]public bool isRaid = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        remainTime = intervalTime;
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        if (!isRaid)
+        {
+            if (remainTime <= 0)
+            {
+                int rand = UnityEngine.Random.Range(0, mobs.Length);
+                SummonMob(mobs[rand]);
+                remainTime = intervalTime;
+            }
+            else
+            {
+                remainTime -= Time.deltaTime;
+            }
+        }
+    }
+
+    protected virtual void SummonMob(GameObject mob)
+    {
+        Instantiate(mob, mob.transform.position, Quaternion.identity);
+    }
+
+    public virtual void SummonBoss()
+    {
+        Instantiate(boss, boss.transform.position, Quaternion.identity);
+    }
+}
